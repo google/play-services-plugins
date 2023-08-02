6a2764fbc13e5a77776948f826d6ab9df1929f7b
David Motsonashvili <davidmotson@google.com>
2023-08-02 09:47:17 -0700

    add typealias for client data

--- google-services-plugin/src/main/kotlin/com/google/gms/googleservices/GoogleServicesTask.kt
+++ google-services-plugin/src/main/kotlin/com/google/gms/googleservices/GoogleServicesTask.kt
@@ -34,6 +34,8 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 
+typealias FirebaseClientData = JsonObject
+
 @CacheableTask
 abstract class GoogleServicesTask : DefaultTask() {
   @get:OutputDirectory abstract val outputDirectory: DirectoryProperty
@@ -99,11 +101,11 @@ abstract class GoogleServicesTask : DefaultTask() {
     handleFirebaseUrl(rootObject, resValues)
     val clientObject = getClientForPackageName(rootObject)
     if (clientObject != null) {
-      handleAnalytics(clientObject, resValues)
-      handleMapsService(clientObject, resValues)
-      handleGoogleApiKey(clientObject, resValues)
-      handleGoogleAppId(clientObject, resValues)
-      handleWebClientId(clientObject, resValues)
+      clientObject.handleAnalytics(resValues)
+      clientObject.handleMapsService(resValues)
+      clientObject.handleGoogleApiKey(resValues)
+      clientObject.handleGoogleAppId(resValues)
+      clientObject.handleWebClientId(resValues)
     } else {
       throw GradleException(
           "No matching client found for package name '${applicationId.get()}' in ${quickstartFile.path}")
@@ -157,93 +159,6 @@ abstract class GoogleServicesTask : DefaultTask() {
     }
   }
 
-  private fun handleWebClientId(clientObject: JsonObject, resValues: MutableMap<String, String?>) {
-    val array = clientObject.getAsJsonArray("oauth_client")
-    if (array != null) {
-      val count = array.size()
-      for (i in 0 until count) {
-        val oauthClientElement = array[i]
-        if (oauthClientElement == null || !oauthClientElement.isJsonObject) {
-          continue
-        }
-        val oauthClientObject = oauthClientElement.asJsonObject
-        val clientType = oauthClientObject.getAsJsonPrimitive("client_type") ?: continue
-        val clientTypeStr = clientType.asString
-        if (OAUTH_CLIENT_TYPE_WEB != clientTypeStr) {
-          continue
-        }
-        val clientId = oauthClientObject.getAsJsonPrimitive("client_id") ?: continue
-        resValues["default_web_client_id"] = clientId.asString
-        return
-      }
-    }
-  }
-
-  /**
-   * Handle a client object for analytics (@xml/global_tracker)
-   *
-   * @param clientObject the client Json object.
-   * @throws IOException
-   */
-  @Throws(IOException::class)
-  private fun handleAnalytics(clientObject: JsonObject, resValues: MutableMap<String, String?>) {
-    val analyticsService = getServiceByName(clientObject, "analytics_service") ?: return
-    val analyticsProp = analyticsService.getAsJsonObject("analytics_property") ?: return
-    val trackingId = analyticsProp.getAsJsonPrimitive("tracking_id") ?: return
-    resValues["ga_trackingId"] = trackingId.asString
-    val xml = File(outputDirectory.get().asFile, "xml")
-    if (!xml.exists() && !xml.mkdirs()) {
-      throw GradleException("Failed to create folder: $xml")
-    }
-    File(xml, "global_tracker.xml")
-        .writeText(getGlobalTrackerContent(trackingId.asString), Charsets.UTF_8)
-  }
-
-  /**
-   * Handle a client object for maps (@string/google_maps_key).
-   *
-   * @param clientObject the client Json object.
-   * @throws IOException
-   */
-  @Throws(IOException::class)
-  private fun handleMapsService(clientObject: JsonObject, resValues: MutableMap<String, String?>) {
-    val mapsService = getServiceByName(clientObject, "maps_service") ?: return
-    val apiKey = getAndroidApiKey(clientObject)
-    if (apiKey != null) {
-      resValues["google_maps_key"] = apiKey
-      return
-    }
-    throw GradleException("Missing api_key/current_key object")
-  }
-
-  private fun handleGoogleApiKey(clientObject: JsonObject, resValues: MutableMap<String, String?>) {
-    val apiKey = getAndroidApiKey(clientObject)
-    if (apiKey != null) {
-      resValues["google_api_key"] = apiKey
-      // TODO: remove this once SDK starts to use google_api_key.
-      resValues["google_crash_reporting_api_key"] = apiKey
-      return
-    }
-    throw GradleException("Missing api_key/current_key object")
-  }
-
-  private fun getAndroidApiKey(clientObject: JsonObject): String? {
-    val array = clientObject.getAsJsonArray("api_key")
-    if (array != null) {
-      val count = array.size()
-      for (i in 0 until count) {
-        val apiKeyElement = array[i]
-        if (apiKeyElement == null || !apiKeyElement.isJsonObject) {
-          continue
-        }
-        val apiKeyObject = apiKeyElement.asJsonObject
-        val currentKey = apiKeyObject.getAsJsonPrimitive("current_key") ?: continue
-        return currentKey.asString
-      }
-    }
-    return null
-  }
-
   /**
    * find an item in the "client" array that match the package name of the app
    *
@@ -269,46 +184,6 @@ abstract class GoogleServicesTask : DefaultTask() {
       }
     }
     return null
-  }
-
-  /** Handle a client object for Google App Id. */
-  @Throws(IOException::class)
-  private fun handleGoogleAppId(clientObject: JsonObject, resValues: MutableMap<String, String?>) {
-    val clientInfo =
-        clientObject.getAsJsonObject("client_info") ?: // Should not happen
-        throw GradleException("Client does not have client info")
-    val googleAppId = clientInfo.getAsJsonPrimitive("mobilesdk_app_id")
-    val googleAppIdStr = googleAppId?.asString
-    if (googleAppIdStr.isNullOrEmpty()) {
-      throw GradleException(
-          "Missing Google App Id. " +
-              "Please follow instructions on https://firebase.google.com/ to get a valid " +
-              "config file that contains a Google App Id")
-    }
-    resValues["google_app_id"] = googleAppIdStr
-  }
-
-  /**
-   * Finds a service by name in the client object. Returns null if the service is not found or if
-   * the service is disabled.
-   *
-   * @param clientObject the json object that represents the client.
-   * @param serviceName the service name
-   * @return the service if found.
-   */
-  private fun getServiceByName(clientObject: JsonObject, serviceName: String): JsonObject? {
-    val services = clientObject.getAsJsonObject("services") ?: return null
-    val service = services.getAsJsonObject(serviceName) ?: return null
-    val status = service.getAsJsonPrimitive("status") ?: return null
-    val statusStr = status.asString
-    if (STATUS_DISABLED == statusStr) return null
-    if (STATUS_ENABLED != statusStr) {
-      logger.warn(
-          String.format(
-              "Status with value '%1\$s' for service '%2\$s' is unknown", statusStr, serviceName))
-      return null
-    }
-    return service
   }
 
   companion object {
@@ -341,5 +216,131 @@ abstract class GoogleServicesTask : DefaultTask() {
       sb.append("</resources>\n")
       return sb.toString()
     }
+  }
+  /** Handle a client object for Google App Id. */
+  @Throws(IOException::class)
+  fun FirebaseClientData.handleGoogleAppId(resValues: MutableMap<String, String?>) {
+    val clientInfo =
+        this.getAsJsonObject("client_info") ?: // Should not happen
+        throw GradleException("Client does not have client info")
+    val googleAppId = clientInfo.getAsJsonPrimitive("mobilesdk_app_id")
+    val googleAppIdStr = googleAppId?.asString
+    if (googleAppIdStr.isNullOrEmpty()) {
+      throw GradleException(
+          "Missing Google App Id. " +
+              "Please follow instructions on https://firebase.google.com/ to get a valid " +
+              "config file that contains a Google App Id")
+    }
+    resValues["google_app_id"] = googleAppIdStr
+  }
+
+  fun FirebaseClientData.handleWebClientId(resValues: MutableMap<String, String?>) {
+    val array = this.getAsJsonArray("oauth_client")
+    if (array != null) {
+      val count = array.size()
+      for (i in 0 until count) {
+        val oauthClientElement = array[i]
+        if (oauthClientElement == null || !oauthClientElement.isJsonObject) {
+          continue
+        }
+        val oauthClientObject = oauthClientElement.asJsonObject
+        val clientType = oauthClientObject.getAsJsonPrimitive("client_type") ?: continue
+        val clientTypeStr = clientType.asString
+        if (OAUTH_CLIENT_TYPE_WEB != clientTypeStr) {
+          continue
+        }
+        val clientId = oauthClientObject.getAsJsonPrimitive("client_id") ?: continue
+        resValues["default_web_client_id"] = clientId.asString
+        return
+      }
+    }
+  }
+
+  /**
+   * Handle a client object for analytics (@xml/global_tracker)
+   *
+   * @param clientObject the client Json object.
+   * @throws IOException
+   */
+  @Throws(IOException::class)
+  private fun FirebaseClientData.handleAnalytics(resValues: MutableMap<String, String?>) {
+    val analyticsService = getServiceByName("analytics_service") ?: return
+    val analyticsProp = analyticsService.getAsJsonObject("analytics_property") ?: return
+    val trackingId = analyticsProp.getAsJsonPrimitive("tracking_id") ?: return
+    resValues["ga_trackingId"] = trackingId.asString
+    val xml = File(outputDirectory.get().asFile, "xml")
+    if (!xml.exists() && !xml.mkdirs()) {
+      throw GradleException("Failed to create folder: $xml")
+    }
+    File(xml, "global_tracker.xml")
+        .writeText(getGlobalTrackerContent(trackingId.asString), Charsets.UTF_8)
+  }
+
+  /**
+   * Handle a client object for maps (@string/google_maps_key).
+   *
+   * @param clientObject the client Json object.
+   * @throws IOException
+   */
+  @Throws(IOException::class)
+  private fun FirebaseClientData.handleMapsService(resValues: MutableMap<String, String?>) {
+    val mapsService = getServiceByName("maps_service") ?: return
+    val apiKey = getAndroidApiKey()
+    if (apiKey != null) {
+      resValues["google_maps_key"] = apiKey
+      return
+    }
+    throw GradleException("Missing api_key/current_key object")
+  }
+
+  private fun FirebaseClientData.handleGoogleApiKey(resValues: MutableMap<String, String?>) {
+    val apiKey = getAndroidApiKey()
+    if (apiKey != null) {
+      resValues["google_api_key"] = apiKey
+      // TODO: remove this once SDK starts to use google_api_key.
+      resValues["google_crash_reporting_api_key"] = apiKey
+      return
+    }
+    throw GradleException("Missing api_key/current_key object")
+  }
+
+  private fun FirebaseClientData.getAndroidApiKey(): String? {
+    val array = this.getAsJsonArray("api_key")
+    if (array != null) {
+      val count = array.size()
+      for (i in 0 until count) {
+        val apiKeyElement = array[i]
+        if (apiKeyElement == null || !apiKeyElement.isJsonObject) {
+          continue
+        }
+        val apiKeyObject = apiKeyElement.asJsonObject
+        val currentKey = apiKeyObject.getAsJsonPrimitive("current_key") ?: continue
+        return currentKey.asString
+      }
+    }
+    return null
+  }
+
+  /**
+   * Finds a service by name in the client object. Returns null if the service is not found or if
+   * the service is disabled.
+   *
+   * @param clientObject the json object that represents the client.
+   * @param serviceName the service name
+   * @return the service if found.
+   */
+  private fun FirebaseClientData.getServiceByName(serviceName: String): JsonObject? {
+    val services = this.getAsJsonObject("services") ?: return null
+    val service = services.getAsJsonObject(serviceName) ?: return null
+    val status = service.getAsJsonPrimitive("status") ?: return null
+    val statusStr = status.asString
+    if (GoogleServicesTask.STATUS_DISABLED == statusStr) return null
+    if (GoogleServicesTask.STATUS_ENABLED != statusStr) {
+      logger.warn(
+          String.format(
+              "Status with value '%1\$s' for service '%2\$s' is unknown", statusStr, serviceName))
+      return null
+    }
+    return service
   }
 }
